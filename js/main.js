c6be815af0121cf35ebe24964d7fe51bcd87c9cd
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-11 23:11:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+function scaleIn() {
+    var element = document.getElementById('myElement');
+    element.style.transition = 'transform 0.3s ease';
+    element.style.transform = 'scale(1.2)';
+
+
+  }
+
+
+
+  function scaleOut() {
+    var element = document.getElementById('myElement');
+    element.style.transition = 'transform 0.3s ease';
+    element.style.transform = 'scale(1)';
+  }
